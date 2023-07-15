1fe69d6d66b433e5414cc43ab30ac8ffbb283f38
Aidan V <ajvastbinder@gmail.com>
2023-07-15 01:10:06 -0600

    done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Excercise> activities = new List<Excercise>();
+        Running running = new Running("Running", "7/15/23", 30, 3);
+        Cycling cycling = new Cycling("Cycling", "7/2/23", 28, 20);
+        Swimming swimming = new Swimming("Swimming", "7/9/23", 22, 11);
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        Console.Clear();
+        Console.WriteLine("--------------");
+        foreach (Excercise excercise in activities)
+        {
+            Console.WriteLine(excercise.GetSummary());
+            Console.WriteLine("--------------");
+        }
     }
 }
